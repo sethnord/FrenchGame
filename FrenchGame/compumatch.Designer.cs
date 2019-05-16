ff77e6aeb936f216b4a0d5119aecaea36b7122ea
Seth Nordahl <sethnord812002@gmail.com>
2019-05-15 23:30:34 -0700

    It is really late.

--- FrenchGame/compumatch.Designer.cs
+++ FrenchGame/compumatch.Designer.cs
@@ -29,75 +29,16 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(compumatch));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.timerLabel = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.timer3 = new System.Windows.Forms.Timer(this.components);
-            this.timer4 = new System.Windows.Forms.Timer(this.components);
             this.playerTime = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Black;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 29);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "XXXXXXXX";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
-            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Black;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(232, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(149, 29);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "XXXXXXXX";
-            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label2_MouseDown);
-            this.label2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label2_MouseUp);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Black;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(452, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 29);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "XXXXXXXX";
-            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label3_MouseDown);
-            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label3_MouseUp);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Black;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(656, 9);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(149, 29);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "XXXXXXXX";
-            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label4_MouseDown);
-            this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label4_MouseUp);
             // 
             // timerLabel
             // 
@@ -105,66 +46,99 @@
             this.timerLabel.BackColor = System.Drawing.Color.Red;
             this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.timerLabel.ForeColor = System.Drawing.Color.White;
-            this.timerLabel.Location = new System.Drawing.Point(866, 6);
+            this.timerLabel.Location = new System.Drawing.Point(631, 17);
             this.timerLabel.Name = "timerLabel";
             this.timerLabel.Size = new System.Drawing.Size(75, 32);
             this.timerLabel.TabIndex = 4;
             this.timerLabel.Text = "0.00";
             // 
-            // timer1
-            // 
-            this.timer1.Interval = 1;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // timer2
-            // 
-            this.timer2.Interval = 1;
-            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
-            // 
-            // timer3
-            // 
-            this.timer3.Interval = 1;
-            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
-            // 
-            // timer4
-            // 
-            this.timer4.Interval = 1;
-            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
-            // 
             // playerTime
             // 
             this.playerTime.Interval = 1;
+            this.playerTime.Tick += new System.EventHandler(this.playerTime_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(254, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(377, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 40);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(497, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(694, 426);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
             // 
             // compumatch
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1094, 651);
+            this.ClientSize = new System.Drawing.Size(718, 908);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.timerLabel);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "compumatch";
             this.Text = "Matching- L\'Ordinateur";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label timerLabel;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Timer timer3;
-        private System.Windows.Forms.Timer timer4;
         private System.Windows.Forms.Timer playerTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- FrenchGame/compumatch.cs
+++ FrenchGame/compumatch.cs
@@ -16,156 +16,39 @@ namespace FrenchGame
 {
     public partial class compumatch : Form
     {
-        int xN = 0;
-        int xT = -1;
-        int yN = 0;
-        int yT = -1;
+
+        float _time = 0.0f;
 
         public compumatch()
         {
             InitializeComponent();
-            MessageBox.Show("In this game, each player will have 5 words in a random order to label an image with. " +
+            MessageBox.Show("In this game, each player will be shown 5 images that they will select the correct word for. " +
                 "Player 1 will go first, player 4 will go last. All players except the player whose turn it is should NOT" +
                 " look at the screen.", "Instructions:", MessageBoxButtons.OK);
+            //Select 5 random images and answers
             LoadFromXML();
+            playerTime.Enabled = true;
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_MouseDown(object sender, MouseEventArgs e)
-        {
-            timer1.Enabled = true;
-        }
-
-        private void label1_MouseUp(object sender, MouseEventArgs e)
-        {
-            timer1.Enabled = false;
-            xT = -1;
-            yT = -1;
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            //Get mouse position
-            xN = Cursor.Position.X;
-            yN = Cursor.Position.Y;
-
-            //Check if we have set the previous position yet, or if this is our first loop
-            if(xT != -1 && yT != -1)
-            {
-                //We have, so move the object a scaled amount
-                int distMoveX = xN - xT;
-                int distMoveY = yN - yT;
-                label1.Left = label1.Left + distMoveX;
-                label1.Top = label1.Top + distMoveY;
-            }
-
-            xT = xN;
-            yT = yN;
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            //Get mouse position
-            xN = Cursor.Position.X;
-            yN = Cursor.Position.Y;
-
-            //Check if we have set the previous position yet, or if this is our first loop
-            if (xT != -1 && yT != -1)
-            {
-                //We have, so move the object a scaled amount
-                int distMoveX = xN - xT;
-                int distMoveY = yN - yT;
-                label2.Left = label2.Left + distMoveX;
-                label2.Top = label2.Top + distMoveY;
-            }
-
-            xT = xN;
-            yT = yN;
-        }
-
-        private void timer3_Tick(object sender, EventArgs e)
-        {
-            //Get mouse position
-            xN = Cursor.Position.X;
-            yN = Cursor.Position.Y;
-
-            //Check if we have set the previous position yet, or if this is our first loop
-            if (xT != -1 && yT != -1)
-            {
-                //We have, so move the object a scaled amount
-                int distMoveX = xN - xT;
-                int distMoveY = yN - yT;
-                label3.Left = label3.Left + distMoveX;
-                label3.Top = label3.Top + distMoveY;
-            }
-
-            xT = xN;
-            yT = yN;
-        }
-
-        private void timer4_Tick(object sender, EventArgs e)
-        {
-            //Get mouse position
-            xN = Cursor.Position.X;
-            yN = Cursor.Position.Y;
-
-            //Check if we have set the previous position yet, or if this is our first loop
-            if (xT != -1 && yT != -1)
-            {
-                //We have, so move the object a scaled amount
-                int distMoveX = xN - xT;
-                int distMoveY = yN - yT;
-                label4.Left = label4.Left + distMoveX;
-                label4.Top = label4.Top + distMoveY;
-            }
-
-            xT = xN;
-            yT = yN;
-        }
-
-        private void label2_MouseDown(object sender, MouseEventArgs e)
-        {
-            timer2.Enabled = true;
-        }
-
-        private void label2_MouseUp(object sender, MouseEventArgs e)
-        {
-            timer2.Enabled = false;
-            xT = -1;
-            yT = -1;
-        }
-
-        private void label3_MouseDown(object sender, MouseEventArgs e)
-        {
-            timer3.Enabled = true;
-        }
-
-        private void label3_MouseUp(object sender, MouseEventArgs e)
-        {
-            timer3.Enabled = false;
-            xT = -1;
-            yT = -1;
-        }
-
-        private void label4_MouseDown(object sender, MouseEventArgs e)
-        {
-            timer4.Enabled = true;
-        }
-
-        private void label4_MouseUp(object sender, MouseEventArgs e)
-        {
-            timer4.Enabled = false;
-            xT = -1;
-            yT = -1;
-        }
+       
 
         private void LoadFromXML()
         {
+            Random r = new Random();
+            int[] imgs = new int[4];
 
+            for (int i = 0; i < 5; i++)
+            {
+                imgs[i] = r.Next(1, 12);
+
+            }
+            XmlDocument qa = new XmlDocument();
+        }
+
+        private void playerTime_Tick(object sender, EventArgs e)
+        {
+            _time += 0.01f;
+            timerLabel.Text = _time.ToString("0.00");
         }
     }
 }
